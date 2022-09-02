a35bcb1f7545134920ae4db12e87286bbb151d2d
Daniel <danil.e@ironwill-et.com>
2022-09-02 19:21:37 +0300

    Implemented the base spawn logic

--- Assets/_Scripts/Core/Board.cs
+++ Assets/_Scripts/Core/Board.cs
@@ -8,7 +8,16 @@ public class Board : MonoBehaviour
     [SerializeField] private Tilemap _tilemap;
     [SerializeField] private Piece _activePiece;
     [SerializeField] private Vector3Int _spawnPosition;
+    private Vector2Int _boardSize = new Vector2Int(10, 20);
 
+    RectInt Bounds
+    {
+        get
+        {
+            var position = new Vector2Int(-_boardSize.x/2, -_boardSize.y / 2);
+            return new RectInt(position, _boardSize);
+        }
+    }
     private void Awake()
     {
         for (var i = 0; i < _tetrominoData.Length; i++)
@@ -27,17 +36,40 @@ public class Board : MonoBehaviour
         var spawnedNumber = Random.Range(0, _tetrominoData.Length);
         var spawnedTetromino = _tetrominoData[spawnedNumber];
 
-        _activePiece.Initialize(this, _spawnPosition, spawnedTetromino);
+        _activePiece.Initialize(this, _spawnPosition, spawnedTetromino); //TODO: interdependence 
 
         Set(_activePiece);
     }
 
-    private void Set(Piece piece)
+    public void Set(Piece piece)
     {
-        foreach (var t in piece.Cells)
+        foreach (var cell in piece.Cells)
         {
-            var tilePosition = t + piece.PiecePosition;
+            var tilePosition = cell + piece.Position;
             _tilemap.SetTile(tilePosition, piece.TetrominoData.Tile);
         }
+    }
+
+    public void Clear(Piece piece) //TODO: Make it automatic after set if it is available
+    {
+        foreach (var cell in piece.Cells)
+        {
+            var tilePosition = cell + piece.Position;
+            _tilemap.SetTile(tilePosition, null);
+        }
+    }
+
+    public bool IsValidPosition(Piece piece, Vector3Int vector3Int)
+    {
+        var bounds = Bounds;
+        foreach (var cell in piece.Cells)
+        {
+            var tilePosition = cell + vector3Int;
+            if(_tilemap.HasTile(tilePosition) ||
+               !bounds.Contains((Vector2Int) tilePosition)) 
+                return false;
+        }
+
+        return true;
     }
 }
--- Assets/_Scripts/Core/Piece.cs
+++ Assets/_Scripts/Core/Piece.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,20 +10,20 @@ public class Piece : MonoBehaviour //TODO: Make it non-monobehaviour
 
     public Vector3Int[] Cells => _cells;
 
-    private Vector3Int _piecePosition;
+    private Vector3Int _position;
 
     public Board Board => _board;
 
-    public Vector3Int PiecePosition => _piecePosition;
+    public Vector3Int Position => _position;
 
     private TetrominoData _tetrominoData;
 
     public TetrominoData TetrominoData => _tetrominoData;
 
-    public void Initialize(Board board,Vector3Int vector3Int, TetrominoData tetrominoData)
+    public void Initialize(Board board, Vector3Int position, TetrominoData tetrominoData)
     {
         _tetrominoData = tetrominoData;
-        _piecePosition = vector3Int;
+        _position = position;
         _board = board;
         _cells ??= new Vector3Int[tetrominoData.Cells.Length];
 
@@ -30,5 +31,39 @@ public class Piece : MonoBehaviour //TODO: Make it non-monobehaviour
         {
             _cells[i] = (Vector3Int) tetrominoData.Cells[i];
         }
+    }
+
+    private void Update()
+    {
+        _board.Clear(this);
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            print("left");
+            Move(Vector2Int.left);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            print("right");
+            Move(Vector2Int.right);
+        }
+
+        _board.Set(this);
+    }
+
+    private bool Move(Vector2Int translation)
+    {
+        var newPosition = _position;
+        newPosition.x +=  translation.x;
+        newPosition.y +=  translation.y;
+
+        var valid = _board.IsValidPosition(this, newPosition);
+        if (valid)
+        {
+            print("is valid");
+            _position = newPosition;
+        }
+
+        return valid;
     }
 }
