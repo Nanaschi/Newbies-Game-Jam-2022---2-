e0e2eeeeace64811b7326e02953e862fed703301
Daniel <danil.e@ironwill-et.com>
2022-09-03 11:07:05 +0300

    Some additions

---
+++ Assets/_Scripts/Core/Extensions.cs
@@ -0,0 +1,14 @@
+﻿namespace _Scripts.Core
+{
+    public static class Extensions
+    {
+        public static int Wrap(this int input, int min, int max)
+        {
+            if (input < min) {
+                return max - (min - input) % (max - min);
+            } else {
+                return min + (input - min) % (max - min);
+            }
+        }
+    }
+}
--- Assets/_Scripts/Core/Piece.cs
+++ Assets/_Scripts/Core/Piece.cs
@@ -1,13 +1,14 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using _Scripts.Core;
 using UnityEngine;
 
 public class Piece : MonoBehaviour //TODO: Make it non-monobehaviour
 {
     private Board _board;
     private Vector3Int[] _cells;
-
+    private int _currentIndex;
     public Vector3Int[] Cells => _cells;
 
     private Vector3Int _position;
@@ -37,11 +38,46 @@ public class Piece : MonoBehaviour //TODO: Make it non-monobehaviour
     {
         _board.Clear(this);
 
+        RotateLogic();
+        MoveLogic();
+        
+        _board.Set(this);
+    }
+
+    private void RotateLogic()
+    {
+        if (Input.GetKeyDown(KeyCode.Q)) Rotate(-1);
+        else if (Input.GetKeyDown(KeyCode.E)) Rotate(1);
+    }
+
+    private void Rotate(int direction)
+    {
+        _currentIndex += direction;
+        _currentIndex.Wrap(0, 4);
+
+        for (int i = 0; i < _cells.Length; i++)
+        {
+            Vector3 cell = _cells[i];
+
+            switch (_tetrominoData.Tetromino)
+            {
+                case (Tetromino.I):
+                case (Tetromino.O):
+                    //.5f logic
+                break;
+                default:
+                    //common logic
+                break;
+            }
+        }
+    }
+
+    private void MoveLogic()
+    {
         if (Input.GetKeyDown(KeyCode.A)) Move(Vector2Int.left);
         else if (Input.GetKeyDown(KeyCode.D)) Move(Vector2Int.right);
         if (Input.GetKeyDown(KeyCode.S)) Move(Vector2Int.down);
         if (Input.GetKeyDown(KeyCode.Space)) HardDrop();
-        _board.Set(this);
     }
 
     private void HardDrop()
